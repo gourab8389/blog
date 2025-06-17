04ab2aa47293ddfed0af36640a6f33a227f01647
gourab8389 <deyg6988@gmail.com>
2025-06-17 22:23:44 +0530

    Add Redis integration for caching blog data and improve blog retrieval logic

--- src/controllers/blog.ts
+++ src/controllers/blog.ts
@@ -1,12 +1,25 @@
+import { redisClient } from "../server.js";
 import { sql } from "../utils/db.js";
 import { TryCatch } from "../utils/try-catch.js";
 import axios from "axios";
-// import dotenv from "dotenv";
-
-// dotenv.config();
 
 export const getAllBlogs = TryCatch(async (req, res) => {
-  const { searchQuery, category } = req.query;
+  const { searchQuery = "", category = "" } = req.query;
+
+  const cachekey = `blogs:${searchQuery}:${category}`;
+
+  const cached = await redisClient.get(cachekey);
+
+  if (cached) {
+    console.log("serving from cache");
+    res.json({
+      status: true,
+      message: "Blogs fetched successfully from cache",
+      blogs: JSON.parse(cached),
+    });
+    return;
+  }
+
   let blogs;
 
   if (searchQuery && category) {
@@ -25,11 +38,23 @@ export const getAllBlogs = TryCatch(async (req, res) => {
     } 
         ORDER BY created_at DESC
         `;
+  } else if (category) {
+    blogs = await sql`
+                SELECT * FROM blogs
+                WHERE category = ${category} ORDER BY created_at DESC
+                `;
   } else {
     blogs = await sql`
         SELECT * FROM blogs ORDER BY created_at DESC
         `;
   }
+
+  console.log("serving from db");
+
+  await redisClient.set(cachekey, JSON.stringify(blogs), {
+    EX: 60 * 60,
+    NX: true,
+  });
 
   res.json({
     status: true,
@@ -41,20 +66,48 @@ export const getAllBlogs = TryCatch(async (req, res) => {
 export const getSingleBlog = TryCatch(async (req, res) => {
   const { id } = req.params;
 
+  const cachekey = `blog:${id}`;
+  const cached = await redisClient.get(cachekey);
+
+  if (cached) {
+    console.log("serving from cache");
+    res.json({
+      status: true,
+      message: "Blog fetched successfully from cache",
+      resposeData: JSON.parse(cached),
+    });
+    return;
+  }
+
   const blog = await sql`
         SELECT * FROM blogs WHERE id = ${id}
         `;
+
+  if (blog.length === 0) {
+    res.status(404).json({
+      status: false,
+      message: "Blog not found",
+    });
+    return;
+  }
 
   const { data } = await axios.get(
     `${process.env.USER_SERVICE}/api/v1/user/${blog[0].author}`
   );
 
+  const resposeData = {
+    blog: blog[0],
+    author: data.user,
+  };
+
+  await redisClient.set(cachekey, JSON.stringify({resposeData: resposeData}), {
+    EX: 60 * 60,
+    NX: true,
+  });
+
   res.json({
     status: true,
     message: "Blog fetched successfully",
-    blog: {
-      blog: blog[0],
-      author: data,
-    },
+    resposeData,
   });
 });
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import blogRoutes from "./routes/blog.js";
+import {createClient} from 'redis'
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient
+.connect()
+.then(()=> console.log("Redis client connected successfully"))
+.catch(console.error);
 
 app.use(express.json());
 
