d6312f35ec48b819a9b05b3cf0250a8e6bbaa590
gourab8389 <deyg6988@gmail.com>
2025-06-17 18:47:49 +0530

    Refactor blog controller to include error handling and improve query logic

--- src/controllers/blog.ts
+++ src/controllers/blog.ts
@@ -1,3 +1,57 @@
-export const getAllBlogs = () => {
+import { sql } from "../utils/db.js";
+import { TryCatch } from "../utils/try-catch.js";
+
+export const getAllBlogs = TryCatch(async (req, res) => {
+  const { searchQuery, category } = req.query;
+  let blogs;
+
+  if (searchQuery && category) {
+    blogs = await sql`
+        SELECT * FROM blogs
+        WHERE (title ILIKE ${"%" + searchQuery + "%"} OR description ILIKE ${
+      "%" + searchQuery + "%"
+    }) AND 
+        category = ${category} ORDER BY created_at DESC
+        `;
+  } else if (searchQuery) {
+    blogs = await sql`
+        SELECT * FROM blogs
+        WHERE title ILIKE ${"%" + searchQuery + "%"} OR description ILIKE ${
+      "%" + searchQuery + "%"
+    } 
+        ORDER BY created_at DESC
+        `;
+  } else {
+    blogs = await sql`
+        SELECT * FROM blogs ORDER BY created_at DESC
+        `;
+  }
+
+  res.json({
+    status: true,
+    message: "Blogs fetched successfully",
+    blogs: blogs,
+  });
+});
+
+export const getSingleBlog = TryCatch(async (req, res) => {
+        const { id } = req.params;
         
-}
+        const blog = await sql`
+        SELECT * FROM blogs WHERE id = ${id}
+        `;
+        
+        if (blog.length === 0) {
+          res.status(404).json({
+            status: false,
+            message: "Blog not found",
+          });
+          return;
+        }
+        
+        res.json({
+        status: true,
+        message: "Blog fetched successfully",
+        blog: blog[0],
+        });
+})
