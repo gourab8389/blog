a18c9e250affa90776b41e6dacc8de90303b3b70
gourab8389 <deyg6988@gmail.com>
2025-06-17 18:13:56 +0530

    Implement blog routes and database connection setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import blogRoutes from "./routes/blog.js";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use("/api/v1", blogRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
